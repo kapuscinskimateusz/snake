30d51335d8c8e00d53be1b519660e385bb76f9cb
m.kapuscinski <kapuscinski.mateusz01@gmail.com>
2024-11-03 23:24:04 +0100

    load campaign levels from json files and display them dynamically

--- js/campaign.js
+++ js/campaign.js
@@ -1,17 +1,17 @@
-import { open as openMainMenu } from "../main-menu.js";
 import { open as openLevelDialog } from "./dialogs/level-dialog.js";
+import { open as openMainMenu } from "../main-menu.js";
 
 const campaign = document.getElementById("campaign");
-const levelBtns = campaign.querySelector(".campaign__levels");
-
-// BUTTONS
+const levelGrid = campaign.querySelector(".campaign__level-grid");
 const backBtn = campaign.querySelector(".campaign__back-btn");
 
-export function open() {
+export async function open() {
   campaign.classList.remove("hidden");
 
+  const levels = await loadLevels();
+  displayLevels(levels);
+
   backBtn.addEventListener("click", handleBack);
-  levelBtns.addEventListener("click", handleLevel);
 }
 
 export function close() {
@@ -20,15 +20,38 @@ export function close() {
   backBtn.removeEventListener("click", handleBack);
 }
 
+async function loadLevels() {
+  try {
+    const response = await fetch("levels.json");
+    if (!response.ok) {
+      throw new Error("Nie udało się załadować poziomów");
+    }
+
+    return await response.json();
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
+
+function clearLevelGrid() {
+  levelGrid.textContent = "";
+}
+
+function displayLevels(levels) {
+  clearLevelGrid();
+
+  levels.forEach((level) => {
+    const levelDiv = document.createElement("div");
+    levelDiv.classList.add("campaign__level");
+    levelDiv.textContent = `Poziom ${level.level}`;
+    levelDiv.addEventListener("click", () => openLevelDialog(level));
+
+    levelGrid.appendChild(levelDiv);
+  });
+}
+
 function handleBack() {
   close();
   openMainMenu();
-}
-
-function handleLevel(event) {
-  const level = event.target
-    .closest(".campaign__level")
-    .getAttribute("data-level");
-
-  openLevelDialog(level);
 }
--- js/dialogs/level-dialog.js
+++ js/dialogs/level-dialog.js
@@ -1,25 +1,40 @@
 import { translate } from "../i18n.js";
+import { startGame } from "../game.js";
+import { close as closeCampaign } from "../campaign.js";
 
 const levelDialog = document.getElementById("level-dialog");
-
-const title = levelDialog.querySelector(".level-dialog__title");
-
-// BUTTONS
+const dialogTitle = levelDialog.querySelector(".level-dialog__title");
+const dialogDescription = levelDialog.querySelector(
+  ".level-dialog__description"
+);
+const startBtn = levelDialog.querySelector(".level-dialog__start-btn");
 const closeBtn = levelDialog.querySelector(".level-dialog__close-btn");
 
-export function open(level) {
-  updateTitle(level);
+export function open(levelObj) {
+  updateLevelInformation(levelObj);
+
   levelDialog.showModal();
 
+  startBtn.addEventListener("click", handleStart);
   closeBtn.addEventListener("click", close);
 }
 
 export function close() {
   levelDialog.close();
 
+  startBtn.removeEventListener("click", handleStart);
   closeBtn.removeEventListener("click", close);
 }
 
-function updateTitle(level) {
-  title.textContent = translate("level") + ` ${level}`;
+function updateLevelInformation(levelObj) {
+  dialogTitle.textContent = translate("level") + ` ${levelObj.level}`;
+  // TODO - translate
+  dialogDescription.textContent = `Cel: Zdobądź ${levelObj.targetScore} punktów.`;
+}
+
+function handleStart() {
+  close();
+  closeCampaign();
+
+  startGame();
 }
